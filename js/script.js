ca555bae7cd593988a606b063cef7d4d676bb94c
Daniel Torres <dnl04trrs@gmail.com>
2024-02-29 22:36:23 -0600

    removed event preview default
    attempt to see if the javascript will work if this line is removed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 document.getElementById('signupForm').addEventListener('submit', function(event) {
-  event.preventDefault();
 
   const fullName = document.getElementById('fullName').value;
 
-  if (!fullName) {
+  if (fullName) {
     alert('Please fill in the full name field.');
   } else {
     alert('Form filled successfully!');
