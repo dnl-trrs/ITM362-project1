40df7a3821316957f9d6bf158bd4d25c3b7b6df7
Daniel Torres <dnl04trrs@gmail.com>
2024-03-21 15:57:26 -0500

    All Spaces Removed
    Don't use random empty lines in your code. At most, use empty lines to separate sections of the code like <head> and <body>, or <sections>.
    
    Corrected, only spaces left are between Head and Body tags

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 document.getElementById('signupForm').addEventListener('submit', function(event) {
-
   const fullName = document.getElementById('fullName').value;
-
   if (!fullName) {
     alert('Please fill in the full name field.');
   } else {
